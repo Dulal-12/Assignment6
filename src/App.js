ba39b10baf67a08c820e0fa5e259d16b4cc36fa2
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-08-27 04:00:38 +0600

    done reactjs project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import userInformation from '../src/component/fakeData/userInformation';
+import'../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Course from './component/course/Course';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //handleButton from here
+  const [count,setCount] = useState([]);
+  const handleUser = (Course) => {
+       const newCount = [...count, Course];
+       setCount(newCount);
+  }
+  let totalPrice = count.reduce((total,cd) => cd.courseFee+total,0);
+
+
+  return ( //navBar section and showing course this section and passing props
+  <div className="container">
+      <nav className="navbar navbar-light bg-dark">
+            <a className="navbar-brand"><h2 className="text-light">Online Course</h2></a>
+       <form className="form-inline">
+             <input className="form-control mr-sm-2" type="search" placeholder="Search Course" aria-label="Search"/>
+             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+   </nav>
+   
+   
+   <div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Your Course </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="card-body" style={{fontFamily: 'Lobster'}}>
+<h5 class="card-title">Total Course : {count.length}</h5>
+    <h5 class="card-text">Total Price : {totalPrice}</h5>
+  </div>
+</div>
+
+   {
+     userInformation.map(user => <Course handleUser={handleUser} user={user}></Course>)
+   }
     </div>
   );
 }
---
+++ src/component/course/Course.js
@@ -0,0 +1,42 @@
+import React from 'react';
+const Course = (props) => {
+
+    //props and destructure here
+    const Course = props.user;
+    const{course,someInformation,teacher,img,email,time,courseFee} = Course;
+
+    //card style
+    const userStyle ={
+                      marginBottom:'40px',
+                      marginTop:"40px",
+                      padding:'10px',
+                      border:'1px solid lightgreen',
+                      borderRadius:'20px',
+    }
+
+    //text style
+    const textStyle={
+        fontWeight:'600',
+    }
+
+
+    return (
+        <div className="card text-center " style={userStyle}>
+                     <div className="card-header" className="p-3 mb-2 bg-success text-white">
+                                 <h2> {course}</h2>
+                     </div>
+                     
+        <div className="card-body" style={textStyle} >
+            <p  className="card-title">{someInformation}</p>
+            <p  className="card-title" ><img src={img} alt="" srcset="" className="col-sm-3 col-md-6 col-lg-8"/></p>
+            <p  className="card-text">Instructor: {teacher}</p>
+            <p  className="card-text">Email Address: {email}</p>
+            <p  className="card-text">Course Duration: {time}</p>
+            <p  className="card-text" style={{fontSize:'25px',color:"purple"}}>Course Fee: ${courseFee}</p>
+            <button className="btn btn-danger" onClick={()=>props.handleUser(Course)}>Enroll Now</button>
+        </div>
+</div>
+    );
+};
+
+export default Course;
